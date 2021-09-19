7af40e8432eb53f3210c8aa0f184e7846e88b120
Shark Desktop (Owen) <owensilva1282@gmail.com>
2021-09-18 22:08:46 -0400

    Release: Setup rollup for building Setup CI/CD Cleanup index.ts

---
+++ rollup.config.js
@@ -0,0 +1,42 @@
+import typescript from 'rollup-plugin-typescript2';
+import replace from '@rollup/plugin-replace';
+
+import pkg from './package.json';
+
+const makeExternalPredicate = (externalArr) => {
+  if (externalArr.length === 0) {
+    return () => false;
+  }
+  const pattern = new RegExp(`^(${externalArr.join('|')})($|/)`);
+  return (id) => pattern.test(id);
+};
+
+export default [
+  // CommonJS (for Node) and ES module (for bundlers) build.
+  {
+    input: 'src/index.ts',
+    external: makeExternalPredicate([
+      ...Object.keys(pkg.dependencies || {}),
+      ...Object.keys(pkg.peerDependencies || {}),
+    ]),
+    plugins: [
+      replace({
+        __name__: pkg.name,
+        __version__: pkg.version,
+      }),
+      typescript({
+        tsconfigOverride: {
+          include: ['./src/**/*'],
+          compilerOptions: {
+            rootDir: 'src',
+            outDir: 'dist',
+          },
+        },
+      }), // so Rollup can convert TypeScript to JavaScript
+    ],
+    output: [
+      {file: pkg.main, format: 'cjs'},
+      {file: pkg.module, format: 'es'},
+    ],
+  },
+];
--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,96 @@
-import { Options } from './types';
+import {Options} from './types';
 
+/**
+ * Represents a database obejct
+ */
 export default class Database {
-    /**
-     * The suffix needed to be appended to the DB url to access json
-     */
-    private readonly URL_SUFFIX = ".json";
+  /**
+   * @type {string}
+   * The suffix needed to be appended to the DB url to access json
+   */
+  private readonly URL_SUFFIX: string = '.json';
 
-    /**
-     * Suffix to be appended to DB URL to access json and do authed requests
-     */
-    private readonly AUTH_SUFFIX = ".json?auth=";
+  /**
+   * @type {string}
+   * Suffix to be appended to DB URL to access json and do authed requests
+   */
+  private readonly AUTH_SUFFIX = '.json?auth=';
 
-    /**
-     * Options for initializing the class look at type def
-     */
-    private options: Options;
+  /**
+   * @type {Options}
+   * Options for initializing the class look at type def
+   */
+  private options: Options;
 
-    constructor(options: Options) {
-        this.options = options;
+  /**
+   * @param {Options} options Config for initializing the lib
+   */
+  constructor(options: Options) {
+    this.options = options;
 
-        if (!options.databaseUrl || options.databaseUrl.length == 0) {
-            console.log('Database URL is invalid. SDK not initialized');
-        }
+    if (!options.databaseUrl || options.databaseUrl.length == 0) {
+      // console.log('Database URL is invalid. SDK not initialized');
     }
+  }
 
-    /**
-     * Writes the given data to the database
-     * @param location Where to write the data
-     * @param data JSON to write, stringified
-     * @param authenticated Should the request be authenticated
-     * @returns If the write operation suceeded
-     */
-    async write(location: string, data: string, authenticated: boolean) {
-        try {
-            await fetch(
-                this.options.databaseUrl + location + 
-                    authenticated ? this.AUTH_SUFFIX + this.options.authentication : this.URL_SUFFIX,
-                {
-                    method: 'PUT',
-                    headers: {
+  /**
+   * Writes the given data to the database
+   * @param {string} location Where to write the data
+   * @param {string} data JSON to write, stringified
+   * @param {HeadersInit} reqHeaders Headers to be sent with the req
+   * @param {boolean} authenticated Should the request be authenticated
+   * @return {boolean} If the write operation suceeded
+   */
+  async write(
+      location: string,
+      data: string,
+      reqHeaders: HeadersInit,
+      authenticated: boolean,
+  ): Promise<boolean> {
+    try {
+      await fetch(
+        this.options.databaseUrl + location + authenticated ?
+          this.AUTH_SUFFIX + this.options.authentication :
+          this.URL_SUFFIX,
+        {
+          method: 'PUT',
+          headers: reqHeaders,
+          body: data,
+        },
+      );
 
-                    },
-                    body: data,
-                },
-            )
-
-            return true;
-        } catch {
-            return false;
-        }
+      return true;
+    } catch {
+      return false;
     }
+  }
+
+  /**
+   * Read data at the given location in DB
+   * @param {string} location Where to read the data
+   * @param {HeadersInit} reqHeaders Headers to be sent with the req
+   * @param {boolean} authenticated Should the request be authenticated
+   * @return {string} JSON or boolean false
+   */
+  async read(
+      location: string,
+      reqHeaders: HeadersInit,
+      authenticated: boolean,
+  ): Promise<string> {
+    try {
+      return await (
+        await fetch(
+          this.options.databaseUrl + location + authenticated ?
+            this.AUTH_SUFFIX + this.options.authentication :
+            this.URL_SUFFIX,
+          {
+            method: 'GET',
+            headers: reqHeaders,
+          },
+        )
+      ).json();
+    } catch {
+      return ''; // Let's not return null...
+    }
+  }
 }
